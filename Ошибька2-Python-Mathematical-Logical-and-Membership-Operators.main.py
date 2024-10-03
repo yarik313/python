e13148e2d3ce6de37025d3dbfcea438603ff9d7b
yarik313 <yakunin.yaroslav.06@gmail.com>
2024-10-03 10:31:29 +0300

    Add files via upload

---
+++ Ошибька2-Python-Mathematical-Logical-and-Membership-Operators.main.py
@@ -0,0 +1,55 @@
+#Задание 1
+a = 5
+b = 10
+a += b
+b = 20
+print("Значение переменной a:", a)
+print("Значение переменной b:", b)
+
+#Задание 2
+x = 7
+y = 3
+z = 0
+sum_result = x + y
+difference_result = x - y
+multiplication_result = x * y
+modulus_result = x % y
+integer_division_result = x // y
+power_result = x ** y
+z = x**2 - (x * y)
+print("Сумма x и y:", sum_result)
+print("Разность x и y:", difference_result)
+print("Произведение x и y:", multiplication_result)
+print("Остаток от деления x на y:", modulus_result)
+print("Целочисленное деление x на y:", integer_division_result)
+print("x в степени y:", power_result)
+print("Значение переменной z:", z
+
+#Задание 3
+m = 8
+n = 4
+m = m + n
+n = m * 2
+m = n % m
+n = n - m
+print("Значение переменной m:", m)
+print("Значение переменной n:", n)
+
+# Задание 4
+a = 12
+b = 8
+c = 12
+print("a больше b:", a > b)
+print("b меньше a:", b < a)
+print("a равно c:", a == c)
+print("b не равно c:", b != c)
+print("a больше или равно c:", a >= c)
+print("b меньше или равно a:", b <= a)
+
+# Задание 5
+backpack = ['ручка', 'тетрадь', 'учебник', 'карандаш', 'фломастеры', 'линейка', 'ластик']
+print("Есть ли в рюкзаке карандаш:", 'карандаш' in backpack)
+print("Нет ли в рюкзаке яблока:", 'яблоко' not in backpack)
+print("Длина рюкзака:", len(backpack))
+
+
