3e0f19cccf80168db77c1cb12d164ac2c98cf567
yarik313 <yakunin.yaroslav.06@gmail.com>
2025-06-28 02:00:55 +0300

    Add files via upload

---
+++ 1-Python-Data-types.py
@@ -0,0 +1,35 @@
+print("Задание 1:")
+inter_var = 1
+float_var = 2.3
+string_var = "github"
+list_var = [1, 2, 3, 4, 5]
+tuple_var1 = (10, 1)
+set_var = {1, 5, 7, 9, 10}
+tuple_var2 = ("coffe", "golf", "hipoham")
+dict_var = {"color": "red", "book": "paper", "car": "BMW"}
+bool_var = True
+print(inter_var, float_var)
+print(string_var)
+print(list_var)
+print(tuple_var1)
+print(set_var)
+print(tuple_var2)
+print(dict_var)
+print(bool_var)
+
+print("Задание 2: (Вывод данных не требовался)")
+name = "yarik"
+age = 18
+average_grade = 4.1
+favorite_movies = ["Бегущий по лезвию", "Большой Лебовски", "Борат", "Брат", "Бумер"]
+personal_info = ("yakunin", 4.1)
+skills = {"learning" : "good", "making money": "not bad"}
+visited_cont = {"Turkey", "Egypt"}
+has_license = False
+
+print("Задание 3:")
+cpu_name = "Intel Core i7-9700K"
+ram_size = 16
+internet_speed = 153.5
+system_info = [cpu_name, ram_size, internet_speed]
+print(system_info)
